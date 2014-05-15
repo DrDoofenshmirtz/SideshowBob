9401cbae277b3f1c58f8bdb7d712c2dfd03d8fcf
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-05-15 08:28:00 +0200

    Fixed config: The boot ns must not be a collection!

--- app-template/app.clj
+++ app-template/app.clj
@@ -1,6 +1,6 @@
 (defapp sideshow-bob
   :ws-service   {:port     17500
-                 :boot     [ssb.welcome]
+                 :boot     ssb.welcome
                  :services [ssb.welcome]}   
   :http-service {:port      20500
                  :root-path "."
