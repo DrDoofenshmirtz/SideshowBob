2ace06b5fab71a777c61a0a204ebc3010893c15b
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-05-16 20:54:04 +0200

    Amended template for a SSB service namespace.
    Demonstrates the basics of defining an SSB service:
    
    * Defines a prefix for routing, i. e. calls will have to be prefixed with
    "welcome." to be routed to this service.
    * Defines an "action" target named "say-hello".
    
    TODO: Add extensive comments to everything.

--- app-template/lib/ssb/welcome.clj
+++ app-template/lib/ssb/welcome.clj
@@ -6,9 +6,14 @@
     :author "Frank Mosebach"}
   ssb.welcome
   (:require 
-    [fm.ssb.boot :as boot]))
+    [fm.ssb.boot :as boot]
+    [fm.websockets.rpc.targets :as tar]))
 
 (boot/def-boot-hook (fn [config]
                       (assoc config ::message 
                                     "Sideshow Bob says: 'Welcome!'")))
+
+(tar/defroute (tar/prefixed-request-name-route "welcome" "."))
+
+(tar/defaction say-hello [])
 
