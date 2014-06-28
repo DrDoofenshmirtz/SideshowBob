237187e5f8678cab96f8c082b32fb3d8cf4b869a
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-06-28 21:23:33 +0200

    Require the config file as script argument.

--- app-template/ssb.sh
+++ app-template/ssb.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env sh
 
+if [ ! -f "$1" ]
+then
+  echo "Usage: ssb.sh config-file"
+  
+  exit 85
+fi
+
 SCRIPT_HOME=$0
 
 if [ -L $SCRIPT_HOME ]; 
@@ -11,5 +18,5 @@ SCRIPT_HOME=`dirname $SCRIPT_HOME`
 
 java -server -Xms32m -Xmx48m -cp "${SCRIPT_HOME}/lib/*" \
 -Djava.util.logging.config.file="${SCRIPT_HOME}/config/logging.properties" \
-fm.ssb.Main "app.clj"
+fm.ssb.Main "$1"
 
